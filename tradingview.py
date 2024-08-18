f8afbd5e92e0431dda83fea6fb8ad4812ffc695f
PC2902 <120700740+PC29002@users.noreply.github.com>
2024-08-18 20:37:06 +0530

    tradingview.py

--- tradingview.py
+++ tradingview.py
@@ -12,7 +12,7 @@ class tradingview_main():
         
         option = webdriver.ChromeOptions()
         option.add_experimental_option("detach",True)
-        #option.add_argument("--headless")
+        option.add_argument("--headless")
         self.driver = webdriver.Chrome(service=Service('E:\\VS code\\Projects\\Seleium\\Selenium 3 Elements\\chromedriver.exe'), options=option)
 
     
@@ -59,67 +59,51 @@ class tradingview_main():
 
         # 1 EMA Triangle and SuperTrend Cross
         #BASE Indicator
-        EMA_Triangle1    = self.driver.find_element(By.XPATH,"/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[3]/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div[1]/div").value_of_css_property("color")
-        EMA_Trianglehex1 = Color.from_string(EMA_Triangle1).hex    
+        EMA_TriangleG    = self.driver.find_element(By.XPATH,"/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[3]/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div").value_of_css_property("color")
+        EMA_TrianglehexG = Color.from_string(EMA_TriangleG).hex    
         
-        EMA_Triangle2    = self.driver.find_element(By.XPATH,"/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[3]/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div").value_of_css_property("color")
-        EMA_Trianglehex2 = Color.from_string(EMA_Triangle2).hex    
-
-        #SuperTrend Cross
-        SuperTrend_Cross    = self.driver.find_element(By.XPATH,"/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[3]/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div[5]/div").value_of_css_property("color")
-        SuperTrend_Crosshex = Color.from_string(SuperTrend_Cross).hex    
+        EMA_TriangleR    = self.driver.find_element(By.XPATH,"/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[3]/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div[3]").value_of_css_property("color")
+        EMA_TrianglehexR = Color.from_string(EMA_TriangleR).hex      
         
-        # SuperTrend 
-        SuperTrend    = self.driver.find_element(By.XPATH,"/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[3]/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div[6]/div").value_of_css_property("color")
-        SuperTrendhex = Color.from_string(SuperTrend).hex    
+        # S-P
+        SuppRes    = self.driver.find_element(By.XPATH,"/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[3]/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div[1]/div").value_of_css_property("color")
+        SuppReshex = Color.from_string(SuppRes).hex    
         
         # 2 RSI Candle and MACD Normal With EMA Triangle 
         #BASE Indicator
-        EMA_Triangle3    = self.driver.find_element(By.XPATH,"/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[5]/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div[1]/div").value_of_css_property("color")
-        EMA_Trianglehex3 = Color.from_string(EMA_Triangle3).hex
+        EMA_TriangleG2    = self.driver.find_element(By.XPATH,"/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[5]/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div[1]/div").value_of_css_property("color")
+        EMA_TrianglehexG2 = Color.from_string(EMA_TriangleG2).hex
         
-        EMA_Triangle4    = self.driver.find_element(By.XPATH,"/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[5]/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div").value_of_css_property("color")
-        EMA_Trianglehex4 = Color.from_string(EMA_Triangle4).hex    
-
-        #MACD Normal
-        MACD_Normal    = self.driver.find_element(By.XPATH,"/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[5]/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div[5]/div").value_of_css_property("color")
-        MACD_Normalhex = Color.from_string(MACD_Normal).hex    
-        
+        EMA_TriangleR2    = self.driver.find_element(By.XPATH,"/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[5]/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div").value_of_css_property("color")
+        EMA_TrianglehexR2 = Color.from_string(EMA_TriangleR2).hex    
         
         print("BTCUSD =>")
-        print("Signal  :", EMA_Trianglehex1, EMA_Trianglehex2, SuperTrend_Crosshex, SuperTrendhex, EMA_Trianglehex3, EMA_Trianglehex4, MACD_Normalhex)
-        self.col_btcusd = [EMA_Trianglehex1, EMA_Trianglehex2, SuperTrend_Crosshex, SuperTrendhex, EMA_Trianglehex3, EMA_Trianglehex4, MACD_Normalhex]
+        print("Signal  :", EMA_TrianglehexG, EMA_TrianglehexR, SuppReshex, EMA_TrianglehexG2, EMA_TrianglehexR2)
+        self.col_btcusd = [EMA_TrianglehexG, EMA_TrianglehexR, SuppReshex, EMA_TrianglehexG2, EMA_TrianglehexR2]
         
     def color_ident_btc(self):
         
         # 1 EMA Triangle and SuperTrend Cross
         b = self.col_btcusd
+        
         # 1st signal
-        if (b[0] == b[2] == "#00ff00") or (b[0] == b[3] == "#00ff00"):
+        #if (b[0] == b[2] == "#00ff00") or (b[3] == "#00ff00"):
+        if (b[0] == b[2] == "#00ff00"):
             #print("BTC Green")
             unit = "BTC Green"
 
 
-        elif (b[1] == b[2] == "#ff0000") or (b[1] == b[3] == "#ff0000"):
+        #elif (b[1] == b[2] == "#ff0000") or (b[4] == "#ff0000"):
+        elif (b[1] == b[2] == "#ff0000"):
             #print("BTC Red")
             unit = "BTC Red"
 
         else:
             #print("avoid")
             unit = "avoid"
+                
         
-        #2 EMA + Macd + RSi Candle
-        if (b[4] == b[6] == "#00ff00"):
-            unit1 = "BTC Green"
-            #print(unit1)
-
-        elif (b[5] == b[6] == "#ff0000"):
-            unit1 = "BTC Red"
-            #print(unit1)
-        else:
-            unit1 = "avoid"
-            #print(unit1)
-        
+        return unit
         # ('BTC Green', 'BTC Green') = output: BTC Green
         # ('BTC Red', 'BTC Red')     = output: BTC Red
         # ('avoid', 'avoid')         = output: avoid
@@ -132,24 +116,6 @@ class tradingview_main():
         # ('BTC Red', 'BTC Green')   = output: avoid
         # ('BTC Green', 'BTC Red')   = output: avoid
                 
-        if unit == unit1:
-            #print(unit)
-            return unit
-
-        elif (unit == 'BTC Red' and unit1 == 'BTC Green') or (unit1 == 'BTC Red' and unit == 'BTC Green'):
-            #print("avoid") 
-            return "avoid"
-
-        elif (unit == 'BTC Red' and unit1 == 'avoid') or (unit == 'BTC Green' and unit1 == 'avoid'):
-            #print(unit)
-            return unit 
-
-        elif (unit1 == 'BTC Red' and unit == 'avoid') or (unit1 == 'BTC Green' and unit == 'avoid'):
-            #print(unit1) 
-            return unit1
-    
-        else:
-            return "avoid"
     
 ###############    ###############  ############### ############### ############### ############### ############### ############### ############### ############### ############### ###############    
     
